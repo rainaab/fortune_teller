0bd828421840479c147c518efcb224eb783d37d1
Raina Bethea <rainabethea@gmail.com>
2025-03-24 21:59:56 -0400

    changing how i declare node and children to allow for a more dynamic tree/ more children of one node

--- fortune_teller.py
+++ fortune_teller.py
@@ -1,41 +1,63 @@
+import random
+
 # using tree nodes to play the old school paper fortune teller game
 
 class Node:
-    def __init__(self,value,left,right):
+    def __init__(self,value):
         self.value = value
-        self.left = left
-        self.right = right
+        self.children = [ ]
+   
+    def add_child(self,child_node):
+        self.children.append(child_node)
 
 # building tree structure starting from smallest level/grandchildren/fortunes
-fortune1 = Node("Good thing you don't get paid for thinkin'! ", None, None)
-fortune2 = Node("Your girlfriend look like my mom!", None, None)
-fortune3 = Node("Lebron James is your KING!!!", None, None)
-fortune4 = Node("Your friends all think you're the best!", None, None)
-fortune5 = Node("I see a hot, rich lifestyle in your future!", None, None)
-fortune6 = Node("You be with J-Money servin' everybody!", None, None)
-fortune7 = Node("No one wants to tell you.... but you stink.", None, None)
-fortune8 = Node("Your mom is HOT!", None, None)
+fortune1 = Node("Good thing you don't get paid for thinkin'! ")
+fortune2 = Node("Your girlfriend look like my mom!")
+fortune3 = Node("Lebron James is your KING!!!")
+fortune4 = Node("Your friends all think you're the best!")
+fortune5 = Node("I see a hot, rich lifestyle in your future!")
+fortune6 = Node("You be with J-Money servin' everybody!")
+fortune7 = Node("No one wants to tell you.... but you stink.")
+fortune8 = Node("Your mom is HOT!")
 # node declarations for middle level/children/numbers
-one = Node(1,None, fortune1)
-two = Node(2,fortune2, None)
-three = Node(3, None ,fortune3)
-four = Node(4,fortune4,None)
-five = Node(5,fortune5,None)
-six = Node(6,None, fortune6)
-seven = Node(7,fortune7, None)
-eight = Node(8,None, fortune8)
+one = Node("1")
+one.add_child(fortune1)
+two = Node("2")
+two.add_child(fortune2)
+three = Node("3")
+three.add_child(fortune3)
+four = Node("4")
+four.add_child(fortune4)
+five = Node("5")
+five.add_child(fortune5)
+six = Node("6")
+six.add_child(fortune6)
+seven = Node("7")
+seven.add_child(fortune7)
+eight = Node("8")
+eight.add_child(fortune8)
 # node declatations for top level/parent/colors
-pink = Node("pink", eight, one)
-purple = Node("purple", two, three)
-green = Node("green", four, five)
-blue = Node("blue", six, seven)
+pink = Node("pink")
+pink.children = [eight,one]
+purple = Node("purple")
+purple.children = [two,three]
+green = Node("green")
+green.children = [four,five]
+blue = Node("blue")
+blue.children = [six,seven]
+# root node
+root = Node("Start")
+root.children = [pink,purple,green,blue]
 
 # code to traverse the tree and make sure nodes are properly linked
-def traversePreOrder(node):
-    if Node is None:
-        return
-    print(node.value, end=" "),
-    if node.left != None:
-        traversePreOrder(node.left)
-    if node.right !=None:
-        traversePreOrder(node.right)
+def print_tree(node, level=0):
+    indent = "  " * level
+    print(f'{indent} - {node.value}')
+    for child in node.children:
+        print_tree(child, level + 1)
+
+# print("Welcome to your future! Warning: These fortunes are a bit unhinged and have a tiktok brainrot theme. have fun! ;)")
+
+# color = input("Please choose a color! ")
+
+print_tree(root)
